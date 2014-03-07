911c053f392bb01ce56115518e1ba7074836c1cf
Rashmi Nair <rashmi.nair@icicletech.com>
2014-03-07 18:47:44 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdvisorFeed::Application.config.secret_key_base = 'a56cb9123114858b4acbce44d385725cba9dcd306d15d380645f970044d1c570581da6ea1791ec22d559e883346441d5926906e389f4ead9226e48d81811acc4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AdvisorFeed::Application.config.session_store :cookie_store, key: '_advisor_feed_session'
