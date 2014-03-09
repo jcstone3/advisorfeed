3ceb5d061a460593413b29007a682627d460877d
Akash Devaraju <akash.devaraju@icicletech.com>
2014-03-09 21:54:13 +0530

    [Admin/Client] Added sample pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 AdvisorFeed::Application.routes.draw do
+  get "admin/index"
+  get "client/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'admin#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
