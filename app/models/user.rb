8790d987d4eac0f09fb1f05bc0ab347f135216e4
Sandeep Sargar <sandeep.sargar@icicletech.com>
2014-03-28 15:46:30 +0530

    validation and mail text changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,18 @@ class User < ActiveRecord::Base
   validates_format_of :last_name, :with =>/^[a-z A-Z][a-z A-Z 0-9_]*$/, :allow_blank => true, :message => "Should contain only alphabets", :multiline => true
   # validates_presence_of :password, :presence => true, :message  => "Can't be blank"
   # validates_length_of :password,  :within => 6..30, :message => "Should be greater than 6 and less than 30"
+
+  validates_presence_of     :password, :if => :password_required?
+  validates_confirmation_of :password, :if => :password_required?
+  validates_length_of       :password, :within => 6..128, :allow_blank => true
+
   validates_presence_of :email, :message => "Can't be blank"
   validates_uniqueness_of :email, :case_sensitive => false, :allow_blank => true, :if => :email_changed?, :message=> "Email address already taken"
   validates_format_of :email, :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?, :message => "Invalid email address"
+
+  protected
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
 end
