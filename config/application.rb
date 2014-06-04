e0325ebd0a2af0b6e068de195ceff71ba0ec2248
Sandeep Sargar <sandeep.sargar@icicletech.com>
2014-06-04 18:27:09 +0530

    squash api details changed

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module AdvisorFeed
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    Squash::Ruby.configure :api_host => 'http://198.199.123.91',
-    :api_key => '526436de-1cbf-42f1-94e0-ef8dfe23abc6',
+    Squash::Ruby.configure :api_host => 'http://ex.icicletech.com/',
+    :api_key => 'c54a4ccc-3dd1-4101-a023-e9d1e8104a6e',
     :disabled => (Rails.env.development? || Rails.env.test?)
   end
 end
