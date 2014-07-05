f04b19477d322bf5c9653ce44bfc5f18822c9d9f
Hitesh Mahajan <hitesh.mahajan@icicletech.com>
2014-07-05 13:43:39 +0530

    added sidekiq.rb and sidekiq.yml

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,12 @@
+require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+  config.redis = { :url => ENV['REDISTOGO_URL'], :size => 5 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { :url => ENV['REDISTOGO_URL'], :size => 10 }
+  config.server_middleware do |chain|
+    #Additional config here
+  end
+  end
