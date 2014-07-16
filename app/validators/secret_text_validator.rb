53e035ea9416ca26a98d4f92d4b81d9ad8e533b3
Rashmi Nair <rashmi.nair@icicletech.com>
2014-07-16 11:51:30 +0530

    [Secret Code] made secret code insensitive

--- app/validators/secret_text_validator.rb
+++ app/validators/secret_text_validator.rb
@@ -3,7 +3,7 @@ class SecretTextValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     #get stored record and compare with the passed value
     user = User.find_by_id(record[:id])
-    unless value == user.secret_text
+    unless value.downcase == user.secret_text.downcase
       record.errors[attribute] << (options[:message] || "Secret Code not matching")
     end
   end
