a2927b47de3f4b9ef3086e472169aa11b4938c0b
Sailee Reddy <sailee.reddy@icicletech.com>
2014-08-12 19:37:43 +0530

    [Sentry] Removed test environment

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -8,7 +8,7 @@ Raven.configure do |config|
   config.logger = logger
   config.current_environment = Rails.env
   # Modify this if your environments are different
-  config.environments = %w[ production staging test ]
+  config.environments = %w[ production staging ]
   config.tags = { environment: Rails.env }
   config.dsn = 'http://3727fed5ee974ad8816e1ebbcd0172a1:fa83b4b6af8d45d1b9e0002af1367a5e@ex.icicletech.com/12'
 end
