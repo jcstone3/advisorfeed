ddc99773f779f8c76c520a47aed1a73594bffe56
Hitesh Mahajan <hitesh.mahajan@icicletech.com>
2015-02-13 15:48:40 +0530

    updated sentry code

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,14 +1,16 @@
 # config/initializers/raven.rb
-require 'raven'
+unless (Rails.env.test? || Rails.env.development?)
+  require 'raven'
 
-logger = ::Logger.new("log/raven.log")
-logger.level = ::Logger::DEBUG
+  logger = ::Logger.new("log/raven.log")
+  logger.level = ::Logger::DEBUG
 
-Raven.configure do |config|
-  config.logger = logger
-  config.current_environment = Rails.env
-  # Modify this if your environments are different
-  config.environments = %w[ production staging ]
-  config.tags = { environment: Rails.env }
-  config.dsn = 'http://3727fed5ee974ad8816e1ebbcd0172a1:fa83b4b6af8d45d1b9e0002af1367a5e@ex.icicletech.com/12'
+  Raven.configure do |config|
+    config.logger = logger
+    config.current_environment = Rails.env
+    # Modify this if your environments are different
+    config.environments = %w[ production staging testing ]
+    config.tags = { environment: Rails.env }
+    config.dsn = 'http://f1cba507756e4fd0b3eb2173f2d85a56:a74d3cdb015543729906221d43a61bb8@ex.icicletech.com/10'
+  end
 end
